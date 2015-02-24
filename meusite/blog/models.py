8c49d1541f8a8dc88a8e4749bb87863936705e68
Willian Galvani <williangalvani@gmail.com>
2015-02-25 00:26:20 +0200

    created basic models

--- meusite/blog/models.py
+++ meusite/blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Poster(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=300)
+    body = models.TextField()
+    author = models.ForeignKey(Poster)
+    date = models.DateTimeField()
