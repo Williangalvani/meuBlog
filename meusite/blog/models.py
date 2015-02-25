2618b5450820219fd48ff76c453cb031de31fb73
Willian Galvani <williangalvani@gmail.com>
2015-02-25 18:54:27 +0200

    added models __str__ fixed edit area size

--- meusite/blog/models.py
+++ meusite/blog/models.py
@@ -4,6 +4,9 @@ class Poster(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField()
 
+    def __str__(self):
+        return "{0} ({1})".format(self.name, self.email)
+
 
 class Post(models.Model):
     title = models.CharField(max_length=300)
@@ -12,3 +15,5 @@ class Post(models.Model):
     author = models.ForeignKey(Poster)
     date = models.DateTimeField()
 
+    def __str__(self):              # __unicode__ on Python 2
+        return '"{0}" by {1} at {2}'.format(self.title, self.author.name, self.date.strftime('%B, %d %Y'))
