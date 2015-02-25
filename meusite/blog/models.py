da372f5566708b8411a8f810b07933f910860ba0
Willian Galvani <williangalvani@gmail.com>
2015-02-25 02:38:07 +0200

    added templates, made them modular.

--- meusite/blog/models.py
+++ meusite/blog/models.py
@@ -1,8 +1,5 @@
 from django.db import models
 
-# Create your models here.
-
-
 class Poster(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField()
@@ -10,6 +7,12 @@ class Poster(models.Model):
 
 class Post(models.Model):
     title = models.CharField(max_length=300)
+    subtitle = models.CharField(max_length=300, null=True)
     body = models.TextField()
     author = models.ForeignKey(Poster)
     date = models.DateTimeField()
+
+    def __init__(self, title, body, *args, **kwargs):
+        super(Post, self).__init__(*args, **kwargs)
+        self.body = body
+        self.title = title
--- meusite/blog/views.py
+++ meusite/blog/views.py
@@ -1,6 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
 from django.http import HttpResponse
+from models import Post
+from django.template import RequestContext
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    posts = Post.objects.order_by('-date')[:5]
+    if not posts:
+        posts= [Post("No posts here!", "")]
+    return render_to_response("index.html",
+                              {"posts": posts},
+                              context_instance=RequestContext(request))
