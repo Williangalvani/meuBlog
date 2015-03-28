2a0f02c6f20605398c7c3f9e193132d107b2a995
Willian Galvani <williangalvani@gmail.com>
2015-03-28 22:56:31 +0200

    converting new app

--- meusite/italia/scripts/searcher.py
+++ meusite/italia/scripts/searcher.py
@@ -98,7 +98,7 @@ class Downloader(threading.Thread):
         threads = []
         self.done = False
         self.stops = []
-        for i in range(2):
+        for i in range(1):
             stop = threading.Event()
             self.stops.append(stop)
             t = DownloadThread(self.name, self,i,stop)
