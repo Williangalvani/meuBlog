6422dc3ec2733cac76681d78ced893ab26d4931d
Willian Galvani <williangalvani@gmail.com>
2015-06-15 17:47:16 +0300

    updated forms to work with latest django

---
+++ meusite/blog/feed.py
@@ -0,0 +1,24 @@
+__author__ = 'will'
+
+
+from django.contrib.syndication.views import Feed
+from django.core.urlresolvers import reverse
+from models import Post
+
+class LatestEntriesFeed(Feed):
+    title = "Latest Posts"
+    link = "/post/"
+    description = ""
+
+    def items(self):
+        return Post.objects.order_by('-date')[:10]
+
+    def item_title(self, item):
+        return item.title
+
+    def item_description(self, item):
+        return item.subtitle
+
+    # item_link is only needed if NewsItem has no get_absolute_url method.
+    def item_link(self, item):
+        return reverse('Post', args=[item.id, ""])
--- meusite/blog/urls.py
+++ meusite/blog/urls.py
@@ -1,12 +1,14 @@
 __author__ = 'will'
 
 from django.conf.urls import patterns, url
-
+from feed import LatestEntriesFeed
 import views
 
 
 urlpatterns = patterns('',
+   url(r'^latest/feed$', LatestEntriesFeed()),
     url(r'^$', views.index, name='index'),
     url(r'^post/(?P<post_id>\d+)/(?P<title>.*)$', views.view_post, name='Post'),
+
 )
 
